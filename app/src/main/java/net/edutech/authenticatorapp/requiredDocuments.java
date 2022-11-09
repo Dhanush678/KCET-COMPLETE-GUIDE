f18435bb9a83956755d54cec53f22b178bdc063b
dhanush678 <dhanugowda200326@gmail.com>
2022-11-09 06:38:47 +0530

    package changed

---
+++ app/src/main/java/net/edutech/authenticatorapp/requiredDocuments.java
@@ -0,0 +1,33 @@
+package net.edutech.authenticatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+import net.edutech.authenticatorapp.utility.NetworkChangeList;
+
+public class requiredDocuments extends AppCompatActivity {
+    NetworkChangeList networkChangeList = new NetworkChangeList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_required_documents);
+    }
+    @Override
+    protected void onStart() {
+        IntentFilter filter =new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeList,filter);
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(networkChangeList);
+        super.onStop();
+    }
+
+}
